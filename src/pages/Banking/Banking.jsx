2e5fbd2a8521c6b40b32b2a65eeac61268ef6640
saksham-tiwari <sakshamt234@gmail.com>
2023-09-08 00:54:47 +0530

    Added banking section

--- src/pages/Banking/Banking.jsx
+++ src/pages/Banking/Banking.jsx
@@ -5,6 +5,25 @@ const Banking = () => {
   return (
     <div id="BankingSection">
       <div className='objWrapperBanking'>
+      <div className='textDiv'>
+        <h2>Banking</h2>
+        <p>Receive your paycheck, tax returns, and other direct deposits up to two days early using your Cash App routing and account number.</p>
+      </div>
+        <img src="/assets/banking-hole.png" alt="bankingHole" className='bankingHole bankingHole1'/>
+        <img src="/assets/banking-hole.png" alt="bankingHole" className='bankingHole bankingHole2'/>
+        <img src="/assets/banking-column.png" alt="bankingColumn" className='bankingColumn'/>
+        <img src="/assets/banking-stairs-1.png" alt="bankingStairs1" className='bankingStairs1'/>
+        <img src="/assets/banking-stairs-2.png" alt="bankingStairs2" className='bankingStairs2'/>
+        <img src="/assets/banking-ramp-1.png" alt="bankingRamp1" className='bankingRamp1'/>
+        <img src="/assets/banking-ramp-2.png" alt="bankingRamp2" className='bankingRamp2'/>
+        <img src="/assets/banking-track-1.png" alt="bankingTrack1" className='bankingTrack1'/>
+        <img src="/assets/banking-track-2.png" alt="bankingTrack2" className='bankingTrack2'/>
+        <img src="/assets/banking-track-2.png" alt="bankingTrack2" className='bankingTrack2'/>
+        <img src="/assets/banking-cubes.png" alt="bankingCubes" className='bankingCubes'/>
+        <img src="/assets/banking-monster.png" alt="bankingMonster" className='bankingMonster'/>
+        <img src="/assets/banking-tube.png" alt="bankingTube" className='bankingTube'/>
+        <img src="/assets/banking-phone.png" alt="bankingPhone" className='bankingPhone'/>
+
       </div>
     </div>
   )
